376a22459dd2ae5e225764da523535f468f7b9a3
Anson Cheung <ansoncwork@gmail.com>
2019-07-05 18:41:05 +0100

    Mount wp-config and remove env variables

--- README.md
+++ README.md
@@ -3,3 +3,28 @@
 For some reasons, this only works if you place your project under C:/ drive. If you place your project in any other drives, it would appear:
 
 ![](https://i.gyazo.com/e0dfd5c1f19874f0a60623c18274248f.png)
+
+# Note 2
+
+When mouting a custom `wp-config.php`, you need to make sure you are not using any environment variable. That's because the offical Wordpress docker image will automatically configure `wp-config.php` using the environment variables you set. 
+
+If you go to localhost it might say **Error establishing a database connection.**, and if you go to `localhost/wp-admin/install.php`. You might see the following message. If you read the message clearly, it says **We were able to connect to the database server**, so all you need to do now is to create a database called `wordpress` 
+
+![](https://gyazo.com/fc3ea3b4dec030d87d7b15d1cd13038b.png)
+
+Personally, I use [HeidiSQL](https://www.heidisql.com/) to connect to the container, and manually create a database called `wordpress`.
+
+![](https://gyazo.com/439d639dc1bbf4e7446a676ec4f7d9a6.gif)
+
+Now if you go back to [localhost](localhost) it should redirect you back to this page.
+![](https://gyazo.com/f0c3a7ae2cb24ce057b08ce20c3c7ba7.png)
+
+# Note 3 (FTP Connection Information!?)
+
+Wordpress cannot write to `wp-content` Because we have mounted it to our host. That's why Wordpress prompt you for your FTP connection information.
+
+To Install Wordpress plugins directly without FTP, you need to add `define('FS_METHOD', 'direct');` to your `wp-config.php`. 
+
+> To understand what security concerns you should have, please read here
+> https://wordpress.stackexchange.com/questions/189554/what-security-concerns-should-i-have-when-setting-fs-method-to-direct-in-wp-co
+
---
+++ wp-config.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+define('FS_METHOD', 'direct');
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'wordpress');
+
+/** MySQL database username */
+define( 'DB_USER', 'root');
+
+/** MySQL database password */
+define( 'DB_PASSWORD', 'dev');
+
+/** MySQL hostname */
+define( 'DB_HOST', 'mysql:3306');
+
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define( 'AUTH_KEY',         '1d9ff055e23577d6baffe78bf7f3b19cba64bb35');
+define( 'SECURE_AUTH_KEY',  'e9b1a7abd6d82c68d8c309990c3c10ab5b91ac1d');
+define( 'LOGGED_IN_KEY',    'd66b81d38ee28dd637a282b6d8ee9f7915c0548f');
+define( 'NONCE_KEY',        '929855d3527b742f4df28e0d821e27b5d37fc9e7');
+define( 'AUTH_SALT',        '486d947e455ecb35ad76e0b14c432bbc46b97e63');
+define( 'SECURE_AUTH_SALT', '829e7399fd2099130401f57634fe432b5eea480e');
+define( 'LOGGED_IN_SALT',   'de0bbfef7cbd422477abb275babbf6a72f76f64b');
+define( 'NONCE_SALT',       'a3d4ed6e2f413bb8f010b9192470293e1cb8072a');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define( 'WP_DEBUG', false );
+
+// If we're behind a proxy server and using HTTPS, we need to alert Wordpress of that fact
+// see also http://codex.wordpress.org/Administration_Over_SSL#Using_a_Reverse_Proxy
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+	$_SERVER['HTTPS'] = 'on';
+}
+
+/* That's all, stop editing! Happy publishing. */
+
+/** Absolute path to the WordPress directory. */
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+/** Sets up WordPress vars and included files. */
+require_once( ABSPATH . 'wp-settings.php' );
