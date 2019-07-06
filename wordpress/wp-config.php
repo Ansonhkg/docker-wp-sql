ba0daa4e922fda54bdd9073961c9e087cc669056
Anson Cheung <ansoncwork@gmail.com>
2019-07-06 21:37:19 +0100

    Add environment variables

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -26,13 +26,13 @@ define('FS_METHOD', 'direct');
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress');
+define( 'DB_NAME', getenv('WORDPRESS_DB_NAME');
 /** MySQL database username */
-define( 'DB_USER', 'root');
+define( 'DB_USER', getenv('WORDPRESS_DB_USER'));
 /** MySQL database password */
-define( 'DB_PASSWORD', 'dev');
+define( 'DB_PASSWORD', getenv('WORDPRESS_DB_PASSWORD'));
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql:3306');
+define( 'DB_HOST', getenv('WORDPRESS_DB_HOST'));
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
